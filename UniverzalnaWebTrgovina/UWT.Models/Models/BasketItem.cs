9affd75150a096c6e3e357cee601ba1b3c945375
Ivan Kovačević <ivankov.ikl@gmail.com>
2015-05-04 22:36:03 +0200

    Database model modified

---
+++ UniverzalnaWebTrgovina/UWT.Models/Models/BasketItem.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using UWT.Models.Interfaces;
+
+namespace UWT.Models {
+    
+    public class BasketItem : IPricing {
+
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+
+        [Required]
+        public virtual Basket Basket { get; set; }
+
+        [Required]
+        public virtual Product Product { get; set; }
+
+        [Required]
+        public double UnitPrice { get; set; }
+    
+    }
+
+}
